c76e3d9e5101ccc5400d1484285d389edc661d9f
MakuMutts <kazminaviktoriya@gmail.com>
2025-12-11 18:26:03 +0300

    v1

---
+++ project/frontend/app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <main style={{ padding: 40, fontFamily: 'system-ui, Arial' }}>
+            <h1>Hi</h1>
+            <p>WORK!!!</p>
+        </main>
+    );
+}
