e8364f3652e9509ae042522c1b44c823691232bf
Максим <panjushinmax@gmail.com>
2026-01-11 18:21:55 +0300

    Create next.config.js

---
+++ frontend/next.config.js
@@ -0,0 +1,31 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['localhost', 'api.autopitch.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.telegram.org',
+      },
+    ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: process.env.NEXT_PUBLIC_API_URL + '/api/:path*',
+      },
+    ]
+  },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+    }
+    return config
+  },
+}
+
+module.exports = nextConfig
